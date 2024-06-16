13f4487e47a52218347dd8e8e921ef6ec07c1f9d
Mateusz Wojtczak <xmateusz.wojtczakx@gmailcom>
2024-06-16 21:51:37 +0200

    Initial Commit

---
+++ algorythm.py
@@ -0,0 +1,14 @@
+wiersz = 0
+tablica=[]
+with open("sygnaly.txt") as file:
+    for linia in file:
+        wiersz= wiersz+1
+        linia = linia.strip()
+        if(wiersz % 40 == 0 ):
+            tablica.append(linia)
+
+
+for i in range(len(tablica)):
+    print(tablica[i][9],end="")
+    wyraz = tablica[i]
+    #print(wyraz[9])
